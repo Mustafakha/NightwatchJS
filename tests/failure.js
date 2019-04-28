1738090287f0afeab4f3bea6148a85c5042a03b2
Mustafa <mustafa.khawaja@mail.mcgill.ca>
2019-04-27 21:21:59 -0400

    modular tests

--- tests/failure.js
+++ tests/failure.js
@@ -1,3 +1,4 @@
+var helper = require('../helpers/helpers.js')
 module.exports = {
     before : function (browser){
       browser.deleteCookies();
@@ -7,32 +8,21 @@ module.exports = {
     },
     tags: ['fail'],
     'GIVEN I am on the web page' : function (browser) {
-         browser 
-         .url('http://testing-ground.scraping.pro/login') // Go to the url
-         .waitForElementVisible('body', 1000) // Wait till page loads
+      helper(browser).navToSite();
     },
     'AND the input fields are present' : function (browser){
-      browser
-         .assert.elementPresent('#usr') //Make sure input field present
-         .assert.elementPresent('#pwd') //Password field present
+      helper(browser).assertFieldsPresent();
     },
    'WHEN I input the wrong values and login' : function (browser){
-     browser
-         .setValue('#usr', 'wrong') // send values
-         .setValue('#pwd', 'wrong') // send values
-         //.element('css selector','#case_login').keys(['\uE007'])
-         .click('#case_login > form > input[type="submit"]:nth-child(5)') // click on login -- for i.e, had to set nativeEvents:false and in windows display settings, size of texts and apps to 100% ?? now works
+      helper(browser).inValidLogin();
    },
    'THEN the page will re load' : function (browser){
-    browser
-         .waitForElementVisible('body', 1000) // wait till page loads
+      helper(browser).pageReloadCheck();
    },
    'AND the text class will be "error" ' : function (browser){
-    browser
-         .assert.cssClassPresent('#case_login > h3',"error") //ensure that the text states "ACCESS DENIED" for failure login
+      helper(browser).checkForFailureText();
        },
    'AND we compare screenshots' : function(browser){
-        browser
-              .compareScreenshot('failure.png') //compare screenshots
+      helper(browser).compareFailureScreenshot();
       }
   };
--- tests/main.js
+++ tests/main.js
@@ -1,3 +1,4 @@
+var helper = require('../helpers/helpers.js')
 module.exports = {
   before : function (browser){
     browser.deleteCookies();
@@ -7,31 +8,21 @@ module.exports = {
   },
   tags: ['main'],
   'GIVEN I am on the web page' : function (browser) {
-       browser 
-       .url('http://testing-ground.scraping.pro/login') // Go to the url
-       .waitForElementVisible('body', 1000) // Wait till page loads
+      helper(browser).navToSite();
   },
   'AND the input fields are present' : function (browser){
-    browser
-       .assert.elementPresent('#usr') //Make sure input field present
-       .assert.elementPresent('#pwd') //Password field present
+      helper(browser).assertFieldsPresent();
   },
  'WHEN I input the correct values and login' : function (browser){
-   browser
-       .setValue('#usr', 'admin') // send values
-       .setValue('#pwd', '12345') // send values
-       .click('#case_login > form > input[type="submit"]:nth-child(5)') // click on login
+      helper(browser).validLogin();
  },
  'THEN the page will re load' : function (browser){
-  browser
-       .waitForElementVisible('body', 1000) // wait till page loads
+      helper(browser).pageReloadCheck();
  },
  'AND the text class will be "success"' : function (browser){
-  browser
-       .assert.cssClassPresent('#case_login > h3',"success") //ensure that the text states "success" for successful login
+      helper(browser).checkForSuccessText();
      },
   'AND we compare screenshots' : function(browser){
-    browser
-          .compareScreenshot('success.png') //compare screenshots
+      helper(browser).compareSuccessfulScreenshot();
   }
 };
